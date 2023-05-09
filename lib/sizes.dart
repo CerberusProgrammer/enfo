4f61b14a8f3de86b6c72a08d16a7426c7b51923e
sazarcode <cerberusprogrammer@gmail.com>
2023-05-09 15:26:59 -0700

    lol

---
+++ lib/sizes.dart
@@ -0,0 +1,7 @@
+class Sizes {
+  static List<double> clock = [];
+  static List<double> mobile = [];
+  static List<double> tablet = [];
+  static List<double> web = [];
+  static List<double> desktop = [];
+}
